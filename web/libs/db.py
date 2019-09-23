1699d152acd59898c0f5af579e86d0f2d485da2d
Jacob G <jacob-x@jvvgindustries.com>
2019-09-23 14:14:23 -0400

    added basic database connection layer

--- web/libs/db.py
+++ web/libs/db.py
@@ -1,5 +1,13 @@
-import mysql.connector
+from MySQLdb import _mysql
 
 class DBConnector:
+	host="localhost"
+	username="bonapp"
+	password="password123"
+	dbname="bonapp"
 	def __init__(self):
-		return
+		self.link=db=_mysql.connect(host, username, password, dbname)
+		
+	def __query(self, query):
+		self.link.query(query)
+		return self.link.use_result()
