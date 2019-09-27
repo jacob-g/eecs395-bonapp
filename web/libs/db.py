8447d1f96c7d864c4d905f8b9855be840b5fc74e
Jacob G <jacob-x@jvvgindustries.com>
2019-09-27 15:49:14 -0400

    adding more database skeleton (specificially reviews and users)
    (#2)

--- web/libs/db.py
+++ web/libs/db.py
@@ -26,6 +26,20 @@ class DBConnector:
 		
 	def menuFor(self, diningHall, date):
 		menuItems = []
-		for row in self.__query("SELECT menu_item.name FROM serves LEFT JOIN menu_item ON menu_item.id=serves.menu_item_id WHERE date_of='%s' AND serves.dining_hall_name='%s' ORDER BY menu_item.name ASC", (date, diningHall.name)):
-			menuItems.append(objects.MenuItem(self.__decode(row[0])))
+		for row in self.__query("SELECT menu_item.id,menu_item.name FROM serves LEFT JOIN menu_item ON menu_item.id=serves.menu_item_id WHERE date_of='%s' AND serves.dining_hall_name='%s' ORDER BY menu_item.name ASC", (date, diningHall.name)):
+			menuItems.append(objects.MenuItem(self.__decode(row[0], row[1])))
 		return menuItems
+		
+	def userFor(self, name):
+		user = []
+		res = self.__query("SELECT id, name FROM user WHERE name='%s' LIMIT 1", (name))
+		if res.len() == 1:
+			return User(res[0], res[1])
+		else:
+			return None
+		
+	def reviewsFor(self, menuItem):
+		reviews = []
+		for row in self.__query("SELECT review.rating,review.comments,item FROM review LEFT JOIN review_of ON review_of.review_id=review.id WHERE review_of.menu_item_id=%s", (menuItem.id)):
+			reviews.append(objects.Review(row[1], row[2]))
+		return reviews
--- web/libs/objects.py
+++ web/libs/objects.py
@@ -2,6 +2,23 @@ class DiningHall:
 	def __init__(self, name):
 		self.name = name
 		
+	def menu(self, date, db):
+		return db.menuFor(self, date)
+		
+	def inventory(self, time):
+		return
+		
 class MenuItem:
-	def __init__(self, name):
+	def __init__(self, id, name):
+		self.id = id
+		self.name = name
+		
+class Review:
+	def __init__(self, rating, comments):
+		self.rating = rating
+		self.comments = comments
+		
+class User:
+	def __init__(self, id, name):
+		self.id = id
 		self.name = name
