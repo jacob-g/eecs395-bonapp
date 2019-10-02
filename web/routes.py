e15e9ca9cde1ab6e3dcd865d1ecec75508e84c37
Jacob G <jacob-x@jvvgindustries.com>
2019-10-02 14:21:20 -0400

    added dynamic list of dining halls to header
    this adds basic templating (closes #3)

--- web/routes.py
+++ web/routes.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 dbLink = DBConnector()
 
+dining_halls = dbLink.diningHalls()
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", dining_halls=dining_halls)
