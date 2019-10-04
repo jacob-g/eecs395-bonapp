0cc7b65369724d6f1da93acdbd621a9b831109f4
Jacob G <jacob-x@jvvgindustries.com>
2019-10-03 20:09:00 -0400

    updated database driver to use official library
    this allows dictionary-based rows to be used (closes #8)

--- web/libs/db.py
+++ web/libs/db.py
@@ -1,4 +1,4 @@
-from MySQLdb import _mysql
+import mysql.connector
 from libs import objects
 
 class DBConnector:
@@ -7,31 +7,38 @@ class DBConnector:
 	password="password"
 	dbname="review"
 	def __init__(self):
-		self.link=_mysql.connect(self.host, self.username, self.password, self.dbname)
+		self.link=mysql.connector.connect(host=self.host, user=self.username, password=self.password, database=self.dbname)
 		
-	#TODO: make this resistant to SQL injection!!!
 	def __query(self, query, args=()):
-		self.link.query(query % args)
-		return self.link.store_result().fetch_row(maxrows=0)
-	
-	def __decode(self, bin):
-		return bin.decode("utf-8")
+		cursor = self.link.cursor()
+		cursor.execute(query, args)
+		return cursor
 		
+	#TODO: factor out some of this logic into a lambda function representing a constructor
 	def diningHalls(self):
 		diningHalls = []
-		for row in self.__query("SELECT name FROM dining_hall ORDER BY name ASC"):
-			diningHalls.append(objects.DiningHall(self.__decode(row[0])))
+		
+		row = {}
+		cursor = self.__query("SELECT name FROM dining_hall ORDER BY name ASC")
+		for row["dining_hall.name"] in cursor:
+			diningHalls.append(objects.DiningHall.from_db(row))
+			
+		cursor.close()
 		return diningHalls
 		
 	def menuFor(self, diningHall, date):
 		menuItems = []
-		for row in self.__query("SELECT menu_item.id,menu_item.name FROM serves LEFT JOIN menu_item ON menu_item.id=serves.menu_item_id WHERE date_of='%s' AND serves.dining_hall_name='%s' ORDER BY menu_item.name ASC", (date, diningHall.name)):
-			menuItems.append(objects.MenuItem(self.__decode(row[0], row[1])))
+		
+		row = {}
+		cursor = self.__query("SELECT menu_item.id,menu_item.name FROM serves LEFT JOIN menu_item ON menu_item.id=serves.menu_item_id WHERE date_of=%s AND serves.dining_hall_name=%s ORDER BY menu_item.name ASC", (date, diningHall.name))
+		for row["menu_item.id"], row["menu_item.name"] in cursor:
+			menuItems.append(objects.MenuItem.from_db(row))
+			
+		cursor.close()
 		return menuItems
 		
 	def userFor(self, name):
-		user = []
-		res = self.__query("SELECT id, name FROM user WHERE name='%s' LIMIT 1", (name))
+		res = self.__query("SELECT id, name FROM user WHERE name=%s LIMIT 1", (name))
 		if res.len() == 1:
 			return objects.User(res[0], res[1])
 		else:
@@ -39,6 +46,11 @@ class DBConnector:
 		
 	def reviewsFor(self, menuItem):
 		reviews = []
-		for row in self.__query("SELECT review.rating,review.comments,item FROM review LEFT JOIN review_of ON review_of.review_id=review.id WHERE review_of.menu_item_id=%s", (menuItem.id)):
-			reviews.append(objects.Review(row[1], row[2]))
+		
+		row = {}
+		cursor = self.__query("SELECT review.rating,review.comments,item FROM review LEFT JOIN review_of ON review_of.review_id=review.id WHERE review_of.menu_item_id=%s", (menuItem.id))
+		for row["review.rating"], row["review.comments"] in cursor:
+			reviews.append(objects.Review.from_db(row))
+			
+		cursor.close()
 		return reviews
--- web/libs/objects.py
+++ web/libs/objects.py
@@ -2,6 +2,10 @@ class DiningHall:
 	def __init__(self, name):
 		self.name = name
 		
+	@staticmethod
+	def from_db(row):
+		return DiningHall(row["dining_hall.name"]);
+		
 	def menu(self, date, db):
 		return db.menuFor(self, date)
 		
@@ -13,10 +17,18 @@ class MenuItem:
 		self.id = id
 		self.name = name
 		
+	@staticmethod
+	def from_db(row):
+		return MenuItem(row["menu_item.id"], row["menu_item.name"])
+		
 class Review:
 	def __init__(self, rating, comments):
 		self.rating = rating
 		self.comments = comments
+	
+	@staticmethod
+	def from_db(row):
+		return Review(row["review.rating"], row["review.comments"])
 
 #TODO: make JSON-serializable
 class User:
