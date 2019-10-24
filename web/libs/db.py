f27496f45aacbfa8fc1d2d1356a5594790271810
Jacob G <jacob-x@jvvgindustries.com>
2019-10-24 14:47:25 -0400

    adding the beginning of unit tests

--- web/libs/db.py
+++ web/libs/db.py
@@ -26,7 +26,7 @@ class DBConnector:
 			
 		return result
 	
-	def __single_row(self, query : str, args : tuple, constructor) -> tuple:
+	def __single_row(self, query : str, args : tuple, constructor):
 		result = self.__query(query, args)
 		if len(result) == 1:
 			return constructor(result[0])
@@ -34,7 +34,7 @@ class DBConnector:
 			return None
 		
 	#TODO: factor out some of this logic into a lambda function representing a constructor
-	def dining_halls(self) -> list[objects.DiningHall]:
+	def dining_halls(self):
 		diningHalls = []
 		
 		row = {}
@@ -43,7 +43,7 @@ class DBConnector:
 			
 		return diningHalls
 	
-	def served_item(self, serves_id : int) -> objects.MenuItemServed:
+	def served_item(self, serves_id : int):
 		result = self.__query("SELECT serves.meal,menu_item.id,menu_item.name,dining_hall.name FROM serves LEFT JOIN menu_item ON menu_item.id=serves.menu_item_id LEFT JOIN dining_hall ON dining_hall.name=serves.dining_hall_name WHERE serves.id=%s ORDER BY menu_item.name ASC", (serves_id,))
 		
 		row = {}
@@ -54,7 +54,7 @@ class DBConnector:
 			return None
 			
 	#TODO: make this take no date by default
-	def menu_for(self, dining_hall : objects.DiningHall, date : datetime.date = datetime.date.today()) -> list[objects.MenuItemServed]:
+	def menu_for(self, dining_hall : objects.DiningHall, date : datetime.date = datetime.date.today()):
 		menu_items = []
 				
 		row = {}
@@ -63,8 +63,8 @@ class DBConnector:
 			
 		return menu_items
 		
-	def user_for(self, name : str) -> objects.User:
-		return self.__single_row("SELECT id, name FROM user WHERE name=%s LIMIT 1", (name,), lambda res : objects.User(res[0], res[1]))
+	def user_for(self, user_id : str):
+		return self.__single_row("SELECT id, name FROM user WHERE id=%s LIMIT 1", (user_id,), lambda res : objects.User(res[0], res[1]))
 		
 	def add_user_if_not_exists(self, user : objects.User):			
 		self.__query("INSERT INTO `user`(id, name) SELECT %s, %s FROM DUAL WHERE (SELECT COUNT(1) FROM user WHERE id=%s)=0", (user.user_id, user.name, user.user_id), True)
@@ -75,7 +75,7 @@ class DBConnector:
 		self.__query("INSERT INTO review(user, rating, comments, item) VALUES(%s, %s, %s, %s)", (user.user_id, rating, comments, serves_id), True)
 		return
 	
-	def allowed_scores(self) -> list[int]:
+	def allowed_scores(self):
 		scores = []
 		
 		for (score,) in self.__query("SELECT score FROM allowed_scores ORDER BY score ASC"):
@@ -83,7 +83,7 @@ class DBConnector:
 			
 		return scores
 	
-	def inventory_for(self, dining_hall : objects.DiningHall, minutes : int) -> list[InventoryItem]:
+	def inventory_for(self, dining_hall : objects.DiningHall, minutes : int):
 		inventories = []
 		
 		row = {}
@@ -92,14 +92,14 @@ class DBConnector:
 		
 		return inventories
 	
-	def inventory_item(self, item_id : int) -> objects.InventoryItem:
+	def inventory_item(self, item_id : int):
 		return self.__single_row("SELECT inventory_item.id,inventory_item.name FROM inventory_item WHERE inventory_item.id=%s", (item_id, ), lambda row : objects.InventoryItem(row[0], row[1]))
 	
 	def add_status(self, dining_hall : objects.DiningHall, inventory_item : objects.InventoryItem, status : int, user : objects.User, minutes : int):
 		self.__query("INSERT INTO statuses(item_id,status,dining_hall,time_stamp,user) SELECT %s, %s, %s, NOW(), %s FROM DUAL WHERE (SELECT COUNT(1) FROM statuses WHERE user=%s AND dining_hall=%s AND item_id=%s AND time_stamp>(NOW() - INTERVAL %s MINUTE))=0", (inventory_item.item_id, status, dining_hall.name, user.user_id, user.user_id, dining_hall.name, inventory_item.item_id, minutes), True)
 		return
 		
-	def reviews_for(self, menu_item : objects.MenuItem) -> list[objects.Review]:
+	def reviews_for(self, menu_item : objects.MenuItem):
 		reviews = []
 		
 		row = {}
@@ -112,7 +112,7 @@ class DBConnector:
 		#TODO: implement this once we have the database structure
 		return None
 	
-	def alerts_for(self, user : objects.User) -> objects.AlertSubscription:
+	def alerts_for(self, user : objects.User):
 		#TODO: implement this once we have the database structure
 		return None
 	
