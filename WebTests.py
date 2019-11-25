f3b209d3d786607529bfee012957c19913b5877a
Divya Manoharan <divyamanoharan@Divyas-Air.attlocal.net>
2019-11-25 17:54:21 -0500

    test that scraper pulls hours and menu items

--- WebTests.py
+++ WebTests.py
@@ -1,16 +1,66 @@
+import sys
+import io
 import unittest
 import lxml.html
 import lxml.etree as etree
 import urllib.request
-#from WebScraper import ______
+import mysql.connector
+from WebScraper import write_to_db, insert_meal, serves_table, insert_hours
 
 class WebTests(unittest.TestCase):
 
-    #create mock leutner menupage
-    page = open("static_menupage.htm", 'r', encoding='utf-8')
-    page_src = page.read()
+    #meals global variables
+    breakfastItems = ""
+    brunchItems = ""
+    lunchItems = ""
+    dinnerItems = ""
 
-    #retrieve page data
-    testtree = etree.parse(page_src)
+    #connect to database
+    connection = mysql.connector.connect(host="localhost", user="bonapp", password="password", database="review")
 
-    
+    def test_read_tree(self):
+        #read mock leutner menupage
+        page = open("static_menupage.htm", 'r', encoding='utf-8')
+
+        #retrieve page data
+        testtree = lxml.html.fromstring(page.read())
+
+        #retrieve hours
+        breakfastHours = testtree.xpath('//section[@data-jump-nav-title="Breakfast"]//div[@class="site-panel__daypart-time"]/text()')
+        brunchHours = testtree.xpath('//section[@data-jump-nav-title="Brunch"]//div[@class="site-panel__daypart-time"]/text()')
+        lunchHours = testtree.xpath('//section[@data-jump-nav-title="Lunch"]//div[@class="site-panel__daypart-time"]/text()')
+        dinnerHours = testtree.xpath('//section[@data-jump-nav-title="Dinner"]//div[@class="site-panel__daypart-time"]/text()')
+
+        #retrieve meals
+        breakfastItems = testtree.xpath('//section[@data-jump-nav-title="Breakfast"]//div[@class="c-tab__content site-panel__daypart-tab-content c-tab__content--active"]//button[@data-js="site-panel__daypart-item-title"]/text()')
+        brunchItems = testtree.xpath('//section[@data-jump-nav-title="Brunch"]//div[@class="c-tab__content site-panel__daypart-tab-content c-tab__content--active"]//button[@data-js="site-panel__daypart-item-title"]/text()')
+        lunchItems = testtree.xpath('//section[@data-jump-nav-title="Lunch"]//div[@class="c-tab__content site-panel__daypart-tab-content c-tab__content--active"]//button[@data-js="site-panel__daypart-item-title"]/text()')
+        dinnerItems = testtree.xpath('//section[@data-jump-nav-title="Dinner"]//div[@class="c-tab__content site-panel__daypart-tab-content c-tab__content--active"]//button[@data-js="site-panel__daypart-item-title"]/text()')
+
+        #test hours scraper
+        self.assertEqual(breakfastHours[0].__str__(), "7:00 am - 10:30 am", "breakfast hours not scraped correctly")
+        self.assertEqual(brunchHours, [], "brunch hours not scraped correctly")
+        self.assertEqual(lunchHours[0].__str__(), "11:00 am - 2:45 pm", "lunch hours not scraped correctly")
+        self.assertEqual(dinnerHours[0].__str__(), "5:00 pm - 8:00 pm", "dinner hours not scraped correctly")
+
+        #test menu_item scraper
+        self.assertEqual(breakfastItems[0].__str__().strip(), "Scrambled Harissa Tofu", "breakfast not scraped correctly")
+        self.assertEqual(brunchItems, [], "brunch not scraped correctly")
+        self.assertEqual(lunchItems[0].__str__().strip(), "Creamy Chicken Paprikash", "lunch not scraped correctly")
+        self.assertEqual(dinnerItems[0].__str__().strip(), "Chicken with Preserved Lemon and Olives", "dinner not scraped correctly")
+
+        #write to database
+        write_to_db(breakfastItems, "Leutner", "Breakfast")
+        write_to_db(brunchItems, "Leutner", "Lunch")
+        write_to_db(lunchItems, "Leutner", "Lunch")
+        write_to_db(dinnerItems, "Leutner", "Dinner")
+
+    #def test_insert_hours(self):
+
+    #def test_insert_meal(self):
+
+    #def test_serves(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
