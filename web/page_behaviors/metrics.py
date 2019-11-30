0930bcef18c7d8a77ad4a509cab3952b0ea431b2
Jacob G <jacob-x@jvvgindustries.com>
2019-11-30 13:27:30 -0500

    added average/total reviews (#16, #75)

--- web/page_behaviors/metrics.py
+++ web/page_behaviors/metrics.py
@@ -1,37 +1,20 @@
 from libs.db import DBConnector
-import plotly
-import plotly.graph_objs as go
-import pandas as pd
-import numpy as np
-import json
-
-
+from datetime import datetime
+from flask import abort
+import simplejson as json
 
 type = "page"
 
-def create_plot():
-
-
-    N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
-
-
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]
-
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return graphJSON
-
-
-def preempt(db : DBConnector, metadata : dict):    
-    return None
+def preempt(db : DBConnector, metadata : dict):
+    if metadata["login_state"].user is None or metadata["login_state"].user.role != "admin":
+        return abort(404) 
+    return    
     
 def page_data(db : DBConnector, metadata : dict):
-    return {"plot": create_plot()}
+    review_stats = db.average_daily_and_total_ratings(datetime.today())
+    
+    menu_item_names = ["%s (%s, %s)" % (review_stat[0].menu_item.name, review_stat[0].dining_hall.name, review_stat[0].meal) for review_stat in review_stats]
+    dailies = [review_stat[0].average_rating for review_stat in review_stats]
+    totals = [review_stat[1] for review_stat in review_stats]
+        
+    return {"menu_items": json.dumps(menu_item_names, use_decimal = True), "dailies": json.dumps(dailies, use_decimal = True), "totals": json.dumps(totals, use_decimal = True)}
