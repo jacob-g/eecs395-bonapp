2c393fc62d2c61376ef338c4caedf87b081292b9
aaabidi <7968706+aaabidi@users.noreply.github.com>
2019-12-02 20:22:55 -0500

    updated

--- web/page_behaviors/metrics.py
+++ web/page_behaviors/metrics.py
@@ -9,29 +9,49 @@ import json
 
 type = "page"
 
-def create_plot():
+def create_food_plot():
 
+    foods=['carrots', 'potatoes', 'cultural appropriation']
 
-    N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+    fig = go.Figure(data=[
+        go.Bar(name='Rating Today', x=foods, y=[3, 4, 2]),
+        go.Bar(name='Average Rating', x=foods, y=[2.5, 4.5, 2])
+    ])
+    # Change the bar mode
+    fig.update_layout(barmode='group',
+                        xaxis_title='Menu Items',
+                        yaxis_title='Rating Score')
 
-
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]
-
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return graphJSON
 
+def create_amenities_plot():
 
-def preempt(db : DBConnector, metadata : dict):    
-    return None
+    timings=['12:00PM','1:00PM','2:00PM','3:00PM','4:00PM']
+    forks = ['3','1','2','2.2','1.5']
+    knives = ['3','3','2.5','3','3']
+    spoons = ['2.5','2','1','1.5','3']
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=timings, y=forks,
+                    mode='lines+markers',
+                    name='forks'))
+    fig.add_trace(go.Scatter(x=timings, y=knives,
+                    mode='lines+markers',
+                    name='knives'))
+    fig.add_trace(go.Scatter(x=timings, y=spoons,
+                    mode='lines+markers',
+                    name='spoons'))
+
+    fig.update_layout(xaxis_title="Amentities", yaxis_title="Availability")
     
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+def preempt(db : DBConnector, metadata : dict):
+    return None
+
 def page_data(db : DBConnector, metadata : dict):
-    return {"plot": create_plot()}
+    return {"food_plot": create_food_plot(), "amenities_plot": create_amenities_plot()}
